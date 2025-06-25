9697ccdc0705c5500a3d0f8151ec3252a9fbfd18
Aditya Singh <adityasingh3781@outlook.com>
2025-06-25 10:05:43 +0530

    Added josephus problem

---
+++ Recursion/Josephus_Problem.java
@@ -0,0 +1,13 @@
+public class Josephus_Problem {
+
+    static int josephus(int n, int k) {
+        if (n == 1) {
+            return 0;
+        }
+        return (josephus(n - 1, k) + k) % n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(josephus(5, 3));
+    }
+}
